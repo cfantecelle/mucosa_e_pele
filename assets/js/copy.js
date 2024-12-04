a1b7304481b3befc6bad080a0163e6a2946dd86b
Carlos Henrique Fantecelle <carloshdfc@gmail.com>
2024-12-04 13:21:43 -0300

    added snackbar and copy function to email button on group page

---
+++ assets/js/copy.js
@@ -0,0 +1,21 @@
+function copyToClipboardWithSnackbar(element) {
+    const textToCopy = element.getAttribute('data-copy');
+    
+    // Copia o texto para a área de transferência
+    navigator.clipboard.writeText(textToCopy).then(
+      function() {
+        // Exibe o snackbar
+        const snackbar = document.getElementById('snackbar');
+        snackbar.classList.add('show');
+        
+        // Esconde o snackbar após 3 segundos
+        setTimeout(function() {
+          snackbar.classList.remove('show');
+        }, 3000);
+      },
+      function(err) {
+        alert("Erro ao copiar texto: " + err);
+      }
+    );
+  }
+  
