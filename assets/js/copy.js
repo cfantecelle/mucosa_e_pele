27b0ef03aa0b0889761ab3dd102eac164f03a511
Carlos Henrique Fantecelle <carloshdfc@gmail.com>
2024-12-10 15:39:29 -0300

    removed carousel js (bugged) and added css provisory animation 60s

--- assets/js/copy.js
+++ assets/js/copy.js
@@ -21,38 +21,6 @@ function copyToClipboardWithSnackbar(element) {
     );
   }
 
-/* Carousel */
-document.addEventListener('DOMContentLoaded', function() {
-  const carousel = document.querySelector('.carousel-images-fp');
-  const images = document.querySelectorAll('.carousel-images-fp img');
-
-  // Duplicar as imagens para criar um loop contínuo
-  images.forEach(img => {
-    const clone = img.cloneNode(true);
-    carousel.appendChild(clone); // Adiciona uma cópia da imagem
-  });
-
-  let width = 0;
-  // Calcular a largura total do carrossel (com todas as imagens)
-  images.forEach(img => {
-    width += img.offsetWidth + 100; // Considerando o gap de 100px
-  });
-
-  // Aplicar a largura total ao container
-  carousel.style.width = width * 2 + 'px'; // Duplicando o comprimento do carrossel
-
-  function moveCarousel() {
-    let currentTransform = parseInt(carousel.style.transform.replace('translateX(', '').replace('px)', '')) || 0;
-    let newTransform = currentTransform - (images[0].offsetWidth + 100); // Move uma imagem por vez
-    if (newTransform <= -width) {
-      newTransform = 0; // Resetar para o início do carrossel
-    }
-    carousel.style.transform = `translateX(${newTransform}px)`;
-  }
-
-  setInterval(moveCarousel, 50); // Move o carrossel a cada 50ms
-});
-
 
 
 
